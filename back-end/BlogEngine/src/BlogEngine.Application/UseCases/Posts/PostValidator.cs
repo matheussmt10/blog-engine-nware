fea41c9df97af95f0aee59df7afb40e3ea836bc8
Matheus Matos <matheusmatosmatheus@gmail.com>
2024-08-18 12:35:55 +0100

    fix: fixed publication date validation

--- back-end/BlogEngine/src/BlogEngine.Application/UseCases/Posts/PostValidator.cs
+++ back-end/BlogEngine/src/BlogEngine.Application/UseCases/Posts/PostValidator.cs
@@ -9,7 +9,7 @@ public class PostValidator : AbstractValidator<RequestCreatePost>
     public PostValidator()
     {
         RuleFor(post => post.Title).NotEmpty().WithMessage(ResourceErrorMessages.TITLE_REQUIRED);
-        RuleFor(post => post.PublicationDate).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.POSTS_CANNOT_FOR_THE_FUTURE);
+        RuleFor(post => post.PublicationDate).NotEmpty().WithMessage(ResourceErrorMessages.PUBLICATION_DATE_REQUIRED).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.POSTS_CANNOT_FOR_THE_FUTURE);
         RuleFor(post => post.Content).NotEmpty().WithMessage(ResourceErrorMessages.CONTENT_REQUIRED);
     }
 }
